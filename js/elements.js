eba793b72c0dfc9ca69ea8df6271ede0153f5a4f
Isabella Corrêa <92992293+IsCorrea@users.noreply.github.com>
2022-10-18 18:25:27 +0100

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -1,4 +1,5 @@
 const btnPlay = document.querySelector('.play')
+const btnPause = document.querySelector('.pause')
 const btnStop = document.querySelector('.stop')
 const btnPlus = document.querySelector('.plus')
 const btnMinus = document.querySelector('.minus')
@@ -9,11 +10,17 @@ const btnFireAudio = document.querySelector('.fire')
 const btnLightMode = document.querySelector('.lightMode')
 const btnDarkMode = document.querySelector('.darkMode')
 
+const rangeVolumeForest = document.querySelector('.volControlForest')
+const rangeVolumeRain = document.querySelector('.volControlRain')
+const rangeVolumeCafe = document.querySelector('.volControlCafe')
+const rangeVolumeFire = document.querySelector('.volControlFire')
+
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 
 export {
   btnPlay,
+  btnPause,
   btnStop,
   btnPlus,
   btnMinus,
@@ -25,4 +32,8 @@ export {
   btnLightMode,
   minutesDisplay,
   secondsDisplay,
+  rangeVolumeForest,
+  rangeVolumeRain,
+  rangeVolumeCafe,
+  rangeVolumeFire,
 }
